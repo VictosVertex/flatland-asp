fe81ac1561a5868aaf9afb4e97b29404f3b26592
VictosVertex <Victos.Vertex@gmail.com>
2023-11-20 15:38:36 +0100

    Add custom log config.

--- src/flatlandasp/main.py
+++ src/flatlandasp/main.py
@@ -1,17 +1,21 @@
 import logging
+from logging.config import dictConfig
 
 import uvicorn
 from fastapi import FastAPI
 
+from flatlandasp.core.log_config import LogConfig
 from flatlandasp.features.router import router as feature_router
 
+dictConfig(LogConfig().dict())
 logger = logging.getLogger("uvicorn.error")
 app = FastAPI(title="FlatlandASP", version="0.01")
 app.include_router(feature_router)
 
 
 def main():
-    uvicorn.run("flatlandasp.main:app", host='0.0.0.0',
+    uvicorn.run("flatlandasp.main:app",
+                host='0.0.0.0',
                 port=8000, reload=True)
 
 
